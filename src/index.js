51c3715fcf4c3907f20ef3eb744bd13a20674ba9
desen <32864798+desen94@users.noreply.github.com>
2021-08-16 16:05:14 +0500

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const lessTwenty = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ]
+    const multiplesTen = [
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ]
+    
+    let thousand = Math.floor(number / 1000)
+    let hundred = Math.floor((number % 1000) / 100)
+    let ten = Math.floor(((number % 1000) % 100) / 10)
+    let unit = (((number % 1000) % 100) % 10)
+    
+    let result = ''
+    
+    if (number === 0) return 'zero'
+    
+    if (thousand > 0) result += toReadable(thousand) + ' thousand '
+    
+    if (hundred > 0) result += lessTwenty[hundred - 1] + ' hundred '
+    
+    if (ten > 1) result += multiplesTen[ten - 2] + ' '
+    else unit += 10 * ten
+    
+    if (unit > 0) result += lessTwenty[unit - 1]
+    
+    return result.trim()
 }
